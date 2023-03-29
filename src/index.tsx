605948897ef5cb2f2fc96e98e845f57cae11261d
Natalia Braz <naabraz@gmail.com>
2023-03-29 09:13:34 -0300

    chore: add normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 
 import './index.css';
 import Cardapio from './pages/Cardapio';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <Cardapio />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
