832171f5bb4293019596eaf87d1e08fb23d5545b
Natalia Braz <naabraz@gmail.com>
2023-03-30 11:52:25 -0300

    chore: add classnames

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,6 +7,7 @@ import Filtros from './Filtros';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
+  const [filtro, setFiltro] = useState<number | null>(null);
 
   return (
     <main>
@@ -25,7 +26,7 @@ export default function Cardapio() {
           setBusca={setBusca}
         />
         <div className={styles.cardapio__filtros}>
-          <Filtros />
+          <Filtros filtro={filtro} setFiltro={setFiltro} />
         </div>
       </section>
     </main>
