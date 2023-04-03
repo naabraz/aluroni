d4f08eb9391accf196371f35c20927e0722e69a1
Natalia Braz <naabraz@gmail.com>
2023-04-03 09:08:41 -0300

    feat: add items component

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -5,6 +5,7 @@ import { ReactComponent as Logo } from 'assets/logo.svg';
 import Buscador from './Buscador';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
+import Itens from './Itens';
 
 export default function Cardapio() {
   const [busca, setBusca] = useState('');
@@ -31,6 +32,7 @@ export default function Cardapio() {
           <Filtros filtro={filtro} setFiltro={setFiltro} />
           <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
         </div>
+        <Itens />
       </section>
     </main>
   )
