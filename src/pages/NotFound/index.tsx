10a09d5225d724bae8997eb0c93d611b5c34ddcd
Natalia Braz <naabraz@gmail.com>
2023-04-07 15:02:46 -0300

    feat: create not found page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { ReactComponent as NotFoundImage } from 'assets/not_found.svg';
+import classNames from 'classnames';
+import styles from './NotFound.module.scss';
+import stylesTema from 'styles/Tema.module.scss';
+
+export default function NotFound() {
+  return (
+    <div
+      className={classNames({
+        [styles.container]: true,
+        [stylesTema.container]: true,
+      })}
+    >
+      <NotFoundImage />
+    </div>
+  );
+}
